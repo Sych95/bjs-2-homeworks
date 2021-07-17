2b6117f36892a95a3e084160c10d7707a6e17f9e
SergejBeschastnov <svb19.09@yandex.ru>
2021-07-17 20:49:24 +0300

    Triangle task added

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,3 +14,45 @@ function validateCount(count) {
       return parseError
     }
 }
+
+class Triangle {
+    constructor(first, second, third) {
+      if(first <= second + third && second <= first + third && third <= first + second){
+        this.first = first,
+        this.second = second,
+        this.third = third
+      } else {
+       const triangleError = new Error('Треугольник с такими сторонами не существует');
+       throw triangleError;
+      }
+    }
+    getPerimeter () {
+        return this.first + this.second + this.third;
+    };
+
+    getArea () {
+        let halfPerimeter  = 0.5 * (this.first + this.second +this.third),
+          areaHeron = Math.sqrt(halfPerimeter * (halfPerimeter - this.first) * (halfPerimeter - this.second) * (halfPerimeter - this.third));
+    
+        return +(areaHeron.toFixed(3)); 
+    }
+}
+
+function getTriangle (a, b, c) {
+    try {
+     return new Triangle(a, b, c);
+    }
+    
+    catch {
+        let wrongTria = {
+            getPerimeter () {
+                return "Ошибка! Треугольник не существует"
+            },
+            getArea () {
+                return "Ошибка! Треугольник не существует"
+            }
+        }
+        return wrongTria
+    }
+}
+  
